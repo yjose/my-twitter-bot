3fa9193c94ffd5f5feefe4f5290bf9eb8a62f2cb
Youssouf Elazizi <youssoufelazizi@gmail.com>
2018-04-29 22:50:20 +0100

    fork from  yjose/twitter-bot

---
+++ index.js
@@ -0,0 +1,5 @@
+const AutoDM = require("./src/AutoDM.js");
+
+console.log("App started successfully 🙌🙌");
+
+AutoDM();
---
+++ src/AutoDM.js
@@ -0,0 +1,48 @@
+const T = require("./Twit.js");
+const my_user_name = require("../config").userName;
+const timeout = 1000 * 60 * 5; // timeout to send the message 5 min
+
+const AutoDM = () => {
+  const stream = T.stream("user");
+  console.log("Start Sending Auto Direct Message 🚀🚀🚀");
+  stream.on("follow", SendMessage);
+};
+
+const SendMessage = user => {
+  console.log(" 🎉🎉🎉🎉 New Follower  🎉🎉🎉🎉🎉 ");
+  const { screen_name, name } = user.source;
+
+  const obj = {
+    screen_name,
+    text: GenerateMessage(name)
+  };
+  // the follow stream track if I follow author person too.
+  if (screen_name != my_user_name) {
+    setTimeout(() => {
+      T.post("direct_messages/new", obj)
+        .catch(err => {
+          console.error("error", err.stack);
+        })
+        .then(result => {
+          console.log(`Message sent successfully To  ${screen_name}  💪💪`);
+        });
+    }, timeout);
+  }
+};
+const GenerateMessage = name => {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  const d = new Date();
+  const dayName = days[d.getDay()];
+  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
+};
+
+module.exports = AutoDM;
